f483a3148f7292c0a3743a0593f1e526ac65deb1
Sachin Khandewal <sachinkhandewal5@gmail.com>
2024-05-13 10:46:30 +0530

    changes

--- RAG.py
+++ RAG.py
@@ -1,4 +1,4 @@
-# my_dag.py
+# RAG.py
 
 import os
 from langchain_experimental.graph_transformers.diffbot import DiffbotGraphTransformer
@@ -15,35 +15,12 @@ def config() -> dict:
     return {"query" : "Warren Buffett",
             "graph_type" : "custom"}
 
-def diffbot_setup() -> ModuleType:
-    diffbot_api_key = "your api key"
-    diffbot_nlp = DiffbotGraphTransformer(diffbot_api_key=diffbot_api_key)
-    return diffbot_nlp
-
-def load_data(diffbot_setup: ModuleType, config: dict) -> list:
-    if config['graph_type'] == "custom":
-        return None
-    query = config['query']
-    # query = "Warren Buffett"
-    # raw_documents = WikipediaLoader(query=query).load()
-    # graph_documents = diff_bot_setup.convert_to_graph_documents(raw_documents)
-    # with open('graph.pkl', 'wb') as f:
-    #     pickle.dump(graph_documents, f)
-    with open('graph.pkl', 'rb') as f:
-        graph_documents = pickle.load(f)
-    return graph_documents
-
-def setup_falkordb(load_data: list, config: dict) -> ModuleType:
-    if config['graph_type'] == "diffbot":
-        graph = FalkorDBGraph("falkordb")
-        graph.add_graph_documents(load_data)
-        graph.refresh_schema()
-        return graph
-    else:
-        graph = FalkorDBGraph("movies")
-        graph.query(
+def setup_falkordb() -> ModuleType:
+    graph = FalkorDBGraph("movies")
+# Populating knowledge graph
+    graph.query(
     """
-    CREATE 
+    CREATE
         (al:Person {name: 'Al Pacino', birthDate: '1940-04-25'}),
         (robert:Person {name: 'Robert De Niro', birthDate: '1943-08-17'}),
         (tom:Person {name: 'Tom Cruise', birthDate: '1962-07-3'}),
@@ -51,10 +28,12 @@ def setup_falkordb(load_data: list, config: dict) -> ModuleType:
         (anthony:Person {name: 'Anthony Edwards', birthDate: '1962-7-19'}),
         (meg:Person {name: 'Meg Ryan', birthDate: '1961-11-19'}),
 
+
         (god1:Movie {title: 'The Godfather'}),
         (god2:Movie {title: 'The Godfather: Part II'}),
         (god3:Movie {title: 'The Godfather Coda: The Death of Michael Corleone'}),
         (top:Movie {title: 'Top Gun'}),
+
 
         (al)-[:ACTED_IN]->(god1),
         (al)-[:ACTED_IN]->(god2),
@@ -67,13 +46,14 @@ def setup_falkordb(load_data: list, config: dict) -> ModuleType:
     """)
     return graph
 
+
 def get_input_query() -> str:
     text = input("Write your query here->")
     return text
 
+
 def get_response_from_openai(setup_falkordb: ModuleType, get_input_query: str) -> str:
-    # graph = setup_falkordb()
-    chain = FalkorDBQAChain.from_llm(OpenAI(temperature=0), 
-                                     graph=setup_falkordb, 
+    chain = FalkorDBQAChain.from_llm(OpenAI(temperature=0),
+                                     graph=setup_falkordb,
                                      verbose=True)
     return chain.run(get_input_query)
--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # Knowledge-graphs-RAG-DAGWorks-Hamilton-FalkorDB-Diffbot-OpenAI
 Creating Knowledge Graphs and Productionalizing your RAG model with using Dagworks, Diffbot, FalkorDB, Langchain and OpenAI
--------------------------------------------------------------------------------------------------------
-#### Install required libraries using:
-
-```python
-pip install -r requirements.txt
-```
--------------------------------------------------------------------------------------------------------
-![](https://github.com/sachink1729/Knowledge-graphs-RAG-DAGWorks-Hamilton-FalkorDB-Diffbot-OpenAI/blob/main/visual.png)
